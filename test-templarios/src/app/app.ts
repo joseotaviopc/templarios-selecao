7441282480fa5b9e2c0ecdcf93b1bb43ce2d15a4
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-07-31 20:23:22 -0300

    fix: styles and format

--- test-templarios/src/app/app.ts
+++ test-templarios/src/app/app.ts
@@ -1,37 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { SidebarComponent } from './components/layout/sidebar/sidebar';
-import { RightSidebarComponent } from './components/layout/right-sidebar/right-sidebar';
-import { MainContentComponent } from './components/layout/main-content/main-content';
+import { Component, signal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, RouterOutlet } from "@angular/router";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { SidebarComponent } from "./components/layout/sidebar/sidebar";
+import { RightSidebarComponent } from "./components/layout/right-sidebar/right-sidebar";
+import { MainContentComponent } from "./components/layout/main-content/main-content";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    CommonModule,
-    RouterModule,
-    RouterOutlet,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    SidebarComponent,
-    RightSidebarComponent,
-    MainContentComponent
-  ],
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+	selector: "app-root",
+	imports: [
+		CommonModule,
+		RouterModule,
+		RouterOutlet,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatIconModule,
+		SidebarComponent,
+		RightSidebarComponent,
+		MainContentComponent,
+	],
+	templateUrl: "./app.html",
+	styleUrls: ["./app.scss"],
 })
 export class App {
-  protected readonly title = signal('test-templarios');
-  isRightSidebarCollapsed = false;
+	protected readonly title = signal("test-templarios");
+	isRightSidebarCollapsed = false;
 
-  toggleRightSidebar() {
-    this.isRightSidebarCollapsed = !this.isRightSidebarCollapsed;
-  }
+	toggleRightSidebar() {
+		this.isRightSidebarCollapsed = !this.isRightSidebarCollapsed;
+	}
 }
