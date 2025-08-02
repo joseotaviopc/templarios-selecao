c51175db8f6e62000ab2627e9f0f932c0d43bcf3
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-08-02 19:16:06 -0300

    feat: create mofal components

---
+++ test-templarios/src/app/components/layout/app-products-modal/app-products-modal.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-products-modal',
+  imports: [MatIconModule, CommonModule],
+  templateUrl: './app-products-modal.html',
+  styleUrl: './app-products-modal.scss'
+})
+export class AppProductsModal {
+  @Input() open = false;
+  @Output() toggleProductsModal = new EventEmitter<void>();
+
+  products = [
+    { id: 1, name: "2022 MEDCURSO", image: "assets/footericon.png" },
+    { id: 2, name: "2022 MED", image: "assets/medicon.png" },
+    { id: 3, name: "2022 RACIPE", image: "assets/racipeicon.png" },
+    { id: 4, name: "2021 CPMED", image: "assets/cpmedicon.png" },
+  ]
+}
---
+++ test-templarios/src/app/components/layout/app-theme-modal/app-theme-modal.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppProductsModal } from './app-theme-modal';
+
+describe('AppProductsModal', () => {
+  let component: AppProductsModal;
+  let fixture: ComponentFixture<AppProductsModal>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppProductsModal]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppProductsModal);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
